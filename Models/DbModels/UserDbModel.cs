276e3e549d0f6b14768d0db4778e9091092dde3c
John <johnpaulfrancis@gmail.com>
2024-10-02 19:42:11 +0530

    Columns of User table

---
+++ Models/DbModels/UserDbModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace JPBank.Models.DbModels
+{
+    public class UserDbModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        public string? Name { get; set; } 
+        [Required]
+        public string? Email { get; set; } 
+        [Required]
+        public string? Password { get; set; }
+        public int UserType { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+    }
+}
