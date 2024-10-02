6be6e9ee406c0d73969736a2dfba2dd667c97914
John <johnpaulfrancis@gmail.com>
2024-10-02 20:20:48 +0530

    Created a controoler and it's view. Set this view as start page.

--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Welcome}/{action=Index}/{id?}");
 
 app.Run();
